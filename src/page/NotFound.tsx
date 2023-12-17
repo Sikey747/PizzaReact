c5cd46bcb16b9041860447920953f42792a3f9e0
Sergey Kudriakova <65175326+Sikey747@users.noreply.github.com>
2023-12-17 16:02:41 +0200

    Update NotFound.tsx

--- src/page/NotFound.tsx
+++ src/page/NotFound.tsx
@@ -15,7 +15,7 @@ function NotFound() {
         </p>
         <img src="../assets/img/empty-cart.png" alt="Empty cart" />
         <a href="/" className="button button--black">
-          <span>Вернуться назад</span>
+          <button type="button">Вернуться назад</span>
         </a>
       </div>
     </Container>
